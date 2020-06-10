6635c52809336e32fc3be75b7897387ecb5010f0
Frank Van Damme <frank.vandamme@gmail.com>
2020-06-10 18:16:01 +0200

    linux mint = apt, dpkg

--- manifests/init.pp
+++ manifests/init.pp
@@ -57,13 +57,13 @@ class etckeeper (
 
   # HIGHLEVEL_PACKAGE_MANAGER config setting.
   $etckeeper_high_pkg_mgr = $::operatingsystem ? {
-    /(?i-mx:ubuntu|debian)/                           => 'apt',
+    /(?i-mx:ubuntu|debian|linuxmint)/                 => 'apt',
     /(?i-mx:centos|fedora|redhat|oraclelinux|amazon)/ => 'yum',
   }
 
   # LOWLEVEL_PACKAGE_MANAGER config setting.
   $etckeeper_low_pkg_mgr = $::operatingsystem ? {
-    /(?i-mx:ubuntu|debian)/                           => 'dpkg',
+    /(?i-mx:ubuntu|debian|linuxmint)/                 => 'dpkg',
     /(?i-mx:centos|fedora|redhat|oraclelinux|amazon)/ => 'rpm',
   }
 
