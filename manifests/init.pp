70805b8046d711c6792a1948e33bdeff823b3ea0
Frank Van Damme <frank.van.damme@vub.be>
2023-02-27 15:21:06 +0100

    refactor legacy facts

--- manifests/init.pp
+++ manifests/init.pp
@@ -56,13 +56,13 @@ class etckeeper (
   }
 
   # HIGHLEVEL_PACKAGE_MANAGER config setting.
-  $etckeeper_high_pkg_mgr = $::operatingsystem ? {
+  $etckeeper_high_pkg_mgr = $facts[os][name] ? {
     /(?i-mx:ubuntu|debian|linuxmint)/                 => 'apt',
     /(?i-mx:centos|fedora|redhat|oraclelinux|amazon)/ => 'yum',
   }
 
   # LOWLEVEL_PACKAGE_MANAGER config setting.
-  $etckeeper_low_pkg_mgr = $::operatingsystem ? {
+  $etckeeper_low_pkg_mgr = $facts[os][name] ? {
     /(?i-mx:ubuntu|debian|linuxmint)/                 => 'dpkg',
     /(?i-mx:centos|fedora|redhat|oraclelinux|amazon)/ => 'rpm',
   }
